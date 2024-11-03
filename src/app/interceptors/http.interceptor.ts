7cd9758301c546b15f82e51d651a20f92cf884b4
Dev Siraj <58474312+Khan-Siraj@users.noreply.github.com>
2024-11-03 23:02:20 +0530

    amend

--- src/app/interceptors/http.interceptor.ts
+++ src/app/interceptors/http.interceptor.ts
@@ -1,5 +1,5 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import { environment } from '../../environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
